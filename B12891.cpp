e5ea3334534f5e3eff139b3e4a4d4a6987b8c119
goeun0422 <jannyeun1004@gmail.com>
2025-09-25 16:32:10 +0900

    Add files via upload

---
+++ B12891.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    char S[1000002], P[1000002];
+
+    int N, M;
+    cin >> N >> M;
+
+    for(int i = 1; i <= N; i++){
+        cin >> S[i];
+    }
+    
+    int A = 0, C = 0, G = 0, T = 0;
+
+    for(int i = 1; i <= M; i++){
+        P[i] = S[i];
+        if(P[i] == 'A'){
+            A++;
+        }
+        if(P[i] == 'C'){
+            C++;
+        }
+        if(P[i] == 'G'){
+            G++;
+        }
+        if(P[i] == 'T'){
+            T++;
+        }
+    }
+
+    int Acount;
+    int Ccount;
+    int Gcount;
+    int Tcount;
+
+    cin >> Acount;
+    cin >> Ccount;
+    cin >> Gcount;
+    cin >> Tcount;
+
+    int result = 0;
+
+    if((Acount <= A)&&(Ccount <= C)&&(Gcount <= G)&&(Tcount <= T))
+    result++;
+
+    for(int i = M + 1; i <= N; i++){
+        if(S[i] == 'A'){
+            A++;
+        }
+        if(S[i] == 'C'){
+            C++;
+        }
+        if(S[i] == 'G'){
+            G++;
+        }
+        if(S[i] == 'T'){
+            T++;
+        }
+        if(S[i - M] == 'A'){
+            A--;
+        }
+        if(S[i - M] == 'C'){
+            C--;
+        }
+        if(S[i - M] == 'G'){
+            G--;
+        }
+        if(S[i - M] == 'T'){
+            T--;
+        }
+
+        if((Acount <= A)&&(Ccount <= C)&&(Gcount <= G)&&(Tcount <= T))
+        result++;
+    }   
+    cout << result;
+    return 0;
+}
